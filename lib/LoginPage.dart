cda61a128d4436b41170ce3ec1d6321cc24e8be0
crreed154 <crreed@wpi.edu>
2026-07-16 13:13:18 -0400

    centered the title for the login page

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -32,8 +32,9 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')
-    
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Login'),
       ),
       body: Center(
         child: SingleChildScrollView(
